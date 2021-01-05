cecbc5fc8b25ba1a4c6f878d8bd50d64d5893408
eslamheshamm <esl2mhesham@gmail.com>
2021-01-05 13:04:49 +0200

    adding import to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import { Test } from "./Test";
 function App() {
-	return (
-		<div className="App  text-center">
-			<h1 className="text-4xl font-bold pt-20">Hello Pickly!</h1>
-		</div>
-	);
+  return (
+    <div className="App  text-center">
+      <h1 className="text-4xl font-bold pt-20">Hello Pickly!</h1>
+
+      <Test />
+    </div>
+  );
 }
 
 export default App;
