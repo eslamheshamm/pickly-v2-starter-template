b90681fb1ea8fc9ed2fefa388ac1bd52ac07065d
eslamheshamm <esl2mhesham@gmail.com>
2021-01-05 13:07:18 +0200

    deleting the test file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { Test } from "./Test";
 function App() {
   return (
     <div className="App  text-center">
       <h1 className="text-4xl font-bold pt-20">Hello Pickly!</h1>
-
-      <Test />
     </div>
   );
 }
